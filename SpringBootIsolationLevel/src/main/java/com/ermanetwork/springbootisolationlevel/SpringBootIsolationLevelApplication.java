ea2012ac30379a90d27c51651e6b8241e09a910b
ermanag <erman.ag@sabancidx.com>
2021-10-19 19:33:22 +0300

    SpringBootIsolationLevelApplication

---
+++ SpringBootIsolationLevel/src/main/java/com/ermanetwork/springbootisolationlevel/SpringBootIsolationLevelApplication.java
@@ -0,0 +1,13 @@
+package com.ermanetwork.springbootisolationlevel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootIsolationLevelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootIsolationLevelApplication.class, args);
+    }
+
+}
---
+++ SpringBootIsolationLevel/src/test/java/com/ermanetwork/springbootisolationlevel/SpringBootIsolationLevelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ermanetwork.springbootisolationlevel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootIsolationLevelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
